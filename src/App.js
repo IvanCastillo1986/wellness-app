8b4f49f218d0aa2f2eb8a0738bcddd56c9115bcb
Ivan Castillo <total180training@gmail.com>
2021-08-21 11:37:54 -0400

    Initial commit. Basic scaffolding , no functionality or css

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+import Results from './Pages/Results'
+import Feedback from './Pages/Feedback'
+import Borough from './Pages/Borough'
+
+
+
+export default function App() {
+
+
+  return (
+    <div>
+      <Navbar />
+      <Switch>
+        <main>
+          <Route exact path='/' component={Home} />
+          <Route path='/results' component={Results} />
+          <Route path='/feedback' component={Feedback} />
+          <Route path='/borough' component={Borough} />
+        </main>
+      </Switch>
+
+      
+    </div>
+  )
+}
---
+++ src/Pages/Borough.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+export default function Borough() {
+
+
+    return (
+        <div>
+            Borough
+        </div>
+    )
+}
