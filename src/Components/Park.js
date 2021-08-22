36b1d61b1539d468491535d4b91898690647a794
Ivan Castillo <total180training@gmail.com>
2021-08-22 18:05:12 -0400

    Pushing finished product before demo

--- src/Components/Park.js
+++ src/Components/Park.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Button, Card, CardGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
-import { CardGroup, Card } from 'react-bootstrap'
 
 
 
 export default function Park({ park, index, emotion }) {
 	return (
-		<div>
-			<CardGroup className='Park'>
-				<Card className='m-5' style={{ width: '18rem' }}>
+		<div className='Park'>
+			<CardGroup>
+				<Card className='m-5' style={{ width: '18rem', textAlign: 'center' }}>
 					<Card.Title className='Park'>
 						<Link to={`/results/${index}`}>
 							<h2>{park.name}</h2>
@@ -22,7 +21,6 @@ export default function Park({ park, index, emotion }) {
 					<Card.Text>
 						{emotion}: {park.emotions[`${emotion}`].value}
 					</Card.Text>
-					<Button variant='success'>Get Directions</Button>
 				</Card>
 			</CardGroup>
 		</div>
