e927f3da10131004513614e239a713aedbf0dd22
EsayH <ez@EsayHs-Mac-mini.local>
2021-08-22 16:06:55 -0400

    add directions btn

--- src/Components/Park.js
+++ src/Components/Park.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Card, CardGroup} from 'react-bootstrap';
+import { Button, Card, CardGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './Park.css'
+import './Park.css';
 
 export default function Park({ park, index, emotion }) {
-
 	return (
-		<Link to={`/results/${index}`}>
-			<CardGroup className="Park">
+		<div>
+			<CardGroup className='Park'>
 				<Card className='m-5' style={{ width: '18rem' }}>
 					<Card.Title className='Park'>
-						<h2>{park.name}</h2>
+						<Link to={`/results/${index}`}>
+							<h2>{park.name}</h2>
+						</Link>
 					</Card.Title>
 					Location:
 					<Card.Text>
@@ -19,8 +19,9 @@ export default function Park({ park, index, emotion }) {
 					<Card.Text>
 						{emotion}: {park.emotions[`${emotion}`].value}
 					</Card.Text>
+					<Button variant='success'>Get Directions</Button>
 				</Card>
 			</CardGroup>
-		</Link>
+		</div>
 	);
 }
